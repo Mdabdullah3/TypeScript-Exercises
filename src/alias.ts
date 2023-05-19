3fbae3e1b8edac2ed5808728f15385bc7c92ec17
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2023-05-19 19:58:21 +0600

    practice done

---
+++ src/alias.ts
@@ -0,0 +1,22 @@
+type HeroIncomeType = number | string;
+const heroIncome:HeroIncomeType = "34";
+
+type HeroType = {
+    name:  string;
+    age: number;
+}
+
+const hero4:HeroType = {
+    name: "Shakib khan",
+    age: 32
+}
+
+const hero5:HeroType = {
+    name: "siam Ahmed",
+    age: 30
+}
+
+const hero6:HeroType = {
+    name: "Ananta jalil",
+    age: 35
+}
